cf696872037e2b3376b3c3fd21ed2d4e4a238ec8
oki3ed... <kokosik3215@gmail.com>
2026-02-23 05:27:01 +0200

    icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/LoadingLayer.hpp>
 #include <Geode/modify/AppDelegate.hpp>
 
 #if defined(_WIN32)
@@ -12,6 +11,35 @@
 #endif
 
 using namespace geode::prelude;
+
+#include <Geode/loader/Dirs.hpp>
+void CopyRuntimeConfigToConfig() {
+    auto runtimeDir = Mod::get::Dirs()->getModRuntimeDir();
+    if (runtimeDir.empty()) return;
+
+    auto runtimeConfig = runtimeDir / "config";
+    auto configDir = Dirs::get()->getConfigDir();
+
+    if (!std::filesystem::exists(runtimeConfig) || 
+        !std::filesystem::is_directory(runtimeConfig))
+        return;
+
+    std::filesystem::create_directories(configDir);
+
+    for (const auto& entry : std::filesystem::recursive_directory_iterator(runtimeConfig)) {
+        if (!entry.is_regular_file()) continue;
+
+        auto relative = std::filesystem::relative(entry.path(), runtimeConfig);
+        auto target = configDir / relative;
+
+        std::filesystem::create_directories(target.parent_path());
+        std::filesystem::copy_file(
+            entry.path(),
+            target,
+            std::filesystem::copy_options::overwrite_existing
+        );
+    }
+}
 
 inline bool fileExists(const std::filesystem::path& path) {
 #if defined(_WIN32)
@@ -75,20 +103,84 @@ inline void updateWindowTitle() {
 #endif
 }
 
+#if defined(_WIN32)
+#include <windows.h>
+#include <gdiplus.h>
+#pragma comment(lib, "gdiplus.lib")
+#endif
+
+inline void updateWindowIcon() {
+    auto iconPath = Mod::get()->getSettingValue<std::filesystem::path>("windowicon");
+
+#if defined(_WIN32)
+    HWND hwnd = GetActiveWindow();
+    if (!hwnd || !fileExists(iconPath)) return;
+
+    static ULONG_PTR gdiplusToken = 0;
+    if (!gdiplusToken) {
+        Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+        Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, nullptr);
+    }
+
+    Gdiplus::Bitmap* bitmap = Gdiplus::Bitmap::FromFile(iconPath.wstring().c_str(), FALSE);
+    if (!bitmap || bitmap->GetLastStatus() != Gdiplus::Ok) {
+        delete bitmap;
+        return;
+    }
+
+    HICON hIcon = nullptr;
+    if (bitmap->GetHICON(&hIcon) == Gdiplus::Ok && hIcon) {
+        SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
+        SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+    }
+
+    delete bitmap;
+
+#else
+    if (!fileExists(iconPath)) return;
+
+    id (*msgSend_id)(id, SEL) = (id (*)(id, SEL))objc_msgSend;
+    id (*msgSend_id_id)(id, SEL, id) = (id (*)(id, SEL, id))objc_msgSend;
+
+    id nsApp = msgSend_id((id)objc_getClass("NSApplication"), sel_getUid("sharedApplication"));
+    id mainWindow = msgSend_id(nsApp, sel_getUid("mainWindow"));
+    if (!mainWindow) return;
+
+    CFStringRef pathStr = CFStringCreateWithCString(NULL, iconPath.string().c_str(), kCFStringEncodingUTF8);
+    id nsPath = (id)CFBridgingRelease(CFURLCreateWithFileSystemPath(NULL, pathStr, kCFURLPOSIXPathStyle, false));
+    CFRelease(pathStr);
+
+    id nsImageClass = (id)objc_getClass("NSImage");
+    id iconImage = msgSend_id_id(nsImageClass, sel_getUid("alloc"), nullptr);
+    iconImage = msgSend_id_id(iconImage, sel_getUid("initWithContentsOfURL:"), nsPath);
+
+    if (iconImage) {
+        msgSend_id_id(mainWindow, sel_getUid("setRepresentedURL:"), nsPath);
+        msgSend_id_id(mainWindow, sel_getUid("setStandardWindowButton:forButtonType:"), iconImage);
+    }
+#endif
+}
+
+#include <Geode/modify/LoadingLayer.hpp>
 class $modify(LoadingLayer) {
     bool init(bool p0) {
         if (!LoadingLayer::init(p0))
             return false;
         updateWindowTitle();
+        updateWindowIcon();
         return true;
     }
 };
 
 $on_mod(Loaded) {
     CopyFromLocal();
+    CopyRuntimeConfigToConfig();
     Mod::get()->loadData();
     geode::listenForSettingChanges("windowname", [](std::string) {
         updateWindowTitle();
+    });
+    geode::listenForSettingChanges("windowicon", [](const std::filesystem::path&) {
+        updateWindowIcon();
     });
 }
 
